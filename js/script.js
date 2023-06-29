3ebe831084039387d1fb909d6146589b0cf9e831
Syed Ahmed <syedahmed@syeds-air.mynetworksettings.com>
2023-06-28 21:22:51 -0400

    Added points when clickin on square

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     const $startButton = $("#start");
     const $squares = $(".square");
+    const $points = $("#points");
 
     //listen for clicks on the start button
     $startButton.on("click", function() {
@@ -24,5 +25,19 @@ $(document).ready(function() {
             $(randomSquare).removeClass('green-bg');
         }, 3000)
     }
+
+    //add Event listeners to our squares
+    $squares.on("click", function(event) {
+       if ($(event.target).hasClass("green-bg")) {
+
+        $(event.target).removeClass("green-bg")
+        // console.log(parseInt($points.text()) + 1)
+        const newPointValue = parseInt($points.text()) + 1;
+        $points.text(newPointValue)
+       } else {
+        console.log("no points for you")
+       }
+
+    })
 
 });
